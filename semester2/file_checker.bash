4a0ee2e376d0db0ccb3eca6aec0e6215e9c008c1
tunji <aadetoro@jhpiego.org>
2025-05-14 10:55:15 +0100

    Added an update on the script

--- semester2/file_checker.bash
+++ semester2/file_checker.bash
@@ -2,7 +2,7 @@
 # Description: This script checks if a file exist in a directory by taking a file name as an argument
 # Date Created: 09-MAY-2025
 # Date Modified: 14-MAY-2025
-# You have to pass a file name as an argument when you want to run the script
+# You have to pass a file name as an argument when you want to run the script: Example = ./file_checker.bash "filename.txt"
 
 # Check if an empty argument is passed
 if [ -z "$1" ]; then
