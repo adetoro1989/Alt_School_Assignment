e9fd92ca3cd5724e7379064235df065fb74a7973
tunji <aadetoro@jhpiego.org>
2025-05-14 08:19:28 +0100

    I modified a comment on the bash script and the date modified

--- semester2/file_checker.bash
+++ semester2/file_checker.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Description: This script checks if a file exist in a directory by taking a file name as an argument
 # Date Created: 09-MAY-2025
-# Date Modified: 09-MAY-2025
-
+# Date Modified: 14-MAY-2025
+# You have to pass a file name as an argument when you want to run the script
 
 # Check if an empty argument is passed
 if [ -z "$1" ]; then
